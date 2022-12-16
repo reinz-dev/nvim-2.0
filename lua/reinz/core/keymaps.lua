16caf2c2e1c2f62c26c85cc9563bcba970ef2a22
reinzcity <reinz.developer@gmail.com>
2022-12-16 14:31:26 +0700

    modified keymaps

--- lua/reinz/core/keymaps.lua
+++ lua/reinz/core/keymaps.lua
@@ -42,7 +42,7 @@ keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window max
 keymap.set("n", "sf", ":NvimTreeToggle<CR>") -- toggle file explorer
 
 -- telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<C-f>", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
@@ -60,5 +60,4 @@ keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if ne
 -- bufferline
 keymap.set("n", "H", "<cmd>BufferLineCyclePrev<CR>")
 keymap.set("n", "L", "<cmd>BufferLineCycleNext<CR>")
-keymap.set("n", "<C-k>", "<cmd>BufferLineCloseLeft<CR><cmd>BufferLineCloseRight<CR>")
-keymap.set("n", "D", "<cmd>BufferLineCloseCurrent<CR>")
+keymap.set("n", "sq", "<cmd>BufferLineCloseLeft<CR><cmd>BufferLineCloseRight<CR>")
